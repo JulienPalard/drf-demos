fb5b5e33c23b27cb5925b23fe680a066023da43f
Julien Palard <julien@palard.fr>
2021-04-24 12:19:13 +0200

    OpenAPI

--- app/urls.py
+++ app/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 import debug_toolbar
 
 urlpatterns = [
@@ -23,4 +25,18 @@ urlpatterns = [
     path("files/", include("file.urls")),
     path("admin/", admin.site.urls),
     path("__debug__/", include(debug_toolbar.urls)),
+    path(
+        "openapi",
+        get_schema_view(
+            title="Demo 1", description="API for all things …", version="1.0.0"
+        ),
+        name="openapi-schema",
+    ),
+    path(
+        "swagger-ui/",
+        TemplateView.as_view(
+            template_name="swagger-ui.html",
+            extra_context={"schema_url": "openapi-schema"},
+        ),
+    ),
 ]
