cf21fbdc9c7372e101c091cb537a0e79aa1b1101
Julien Palard <julien@palard.fr>
2021-06-02 21:13:55 +0200

    Proper admin for users.

--- uptime/admin.py
+++ uptime/admin.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 import uptime.models
 
 admin.site.register(uptime.models.Check)
 admin.site.register(uptime.models.Domain)
-admin.site.register(uptime.models.User)
+admin.site.register(uptime.models.User, UserAdmin)
