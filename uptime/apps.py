1150f9887a191b9145770e3dea5206c698a72100
Julien Palard <julien@palard.fr>
2021-06-03 14:39:01 +0200

    Passage de black.

--- uptime/apps.py
+++ uptime/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class UptimeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'uptime'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "uptime"
