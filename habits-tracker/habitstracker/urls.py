f92520c21be96cf9c68f24909e14fabd9bf53a2b
Julien Palard <julien@palard.fr>
2021-11-24 09:51:07 +0100

    Add some fake dones.

--- habits-tracker/habitstracker/urls.py
+++ habits-tracker/habitstracker/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path, include
-from rest_framework import routers
 from habitstracker import views
+from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
 router.register("users", views.UserViewSet)
 router.register("habits", views.HabitsViewSet, basename="habits")
 
+habits_router = routers.NestedSimpleRouter(
+    parent_router=router, parent_prefix="habits", lookup="habit"
+)
+habits_router.register("done", views.DoneViewSet, basename="habits-done")
+
+
+print(*router.urls, sep="\n")
+print(*habits_router.urls, sep="\n")
+
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(habits_router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
--- habits-tracker/habitstracker/views.py
+++ habits-tracker/habitstracker/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.reverse import reverse
 from rest_framework import permissions
@@ -34,6 +32,17 @@ def fake_habit(pk, request):
         "name": choice(habits),
         "qty": randint(1, 10),
         "interval": choice(("yearly", "monthly", "weekly", "daily")),
+        "done": reverse("habits-done-list", args=[pk], request=request),
+    }
+
+
+def fake_done(habit_pk, pk, request):
+    from random import randint
+    from datetime import datetime, timedelta
+
+    return {
+        "url": reverse("habits-done-detail", args=[habit_pk, pk], request=request),
+        "at": (datetime.now() - timedelta(seconds=randint(100, 100_000))).isoformat(),
     }
 
 
@@ -47,3 +56,17 @@ class HabitsViewSet(viewsets.ViewSet):
     def retrieve(self, request, pk=None):
         """Provide an Habit."""
         return Response(fake_habit(pk, request))
+
+
+class DoneViewSet(viewsets.ViewSet):
+    """A fake viewset to give fake habits."""
+
+    def list(self, request, habit_pk):
+        """Provide Habits."""
+        return Response(
+            {"items": [fake_done(habit_pk, pk, request) for pk in range(1, 4)]}
+        )
+
+    def retrieve(self, request, habit_pk, pk=None):
+        """Provide an Habit."""
+        return Response(fake_done(habit_pk, pk, request))
